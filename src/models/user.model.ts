51066c66dc7d42f810496dfcf4296de2a000e7aa
Arti-Mdhr <artimanandhar@gmail.com>
2026-01-05 22:49:29 +0545

    user model modified

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,23 +1,75 @@
-import mongoose from "mongoose";
+import { Request, Response } from "express";
+import { UserService } from "../services/user.service";
+import { registerDto, loginDto } from "../dtos/user.dto";
+import z from "zod";
 
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true, // ensures no duplicate emails
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    role: {
-      type: String,
-      enum: ["user", "admin"], // teacher requirement
-      default: "user",
-    },
-  },
-  { timestamps: true } // adds createdAt & updatedAt
-);
+const service = new UserService();
 
-export const UserModel = mongoose.model("User", userSchema);
+export class AuthController {
+  // REGISTER
+  async register(req: Request, res: Response) {
+    try {
+      // Validate request body
+      const parsed = registerDto.safeParse(req.body);
+      if (!parsed.success) {
+        return res.status(400).json({
+          success: false,
+          message: z.prettifyError(parsed.error),
+        });
+      }
+
+      const data = parsed.data;
+
+      // Call service
+      const user = await service.register(data.email, data.password);
+
+      // Send response (omit password)
+      return res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        data: {
+          id: user._id,
+          email: user.email,
+          role: user.role,
+        },
+      });
+    } catch (error: any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal Server Error",
+      });
+    }
+  }
+
+  // LOGIN
+  async login(req: Request, res: Response) {
+    try {
+      // Validate request body
+      const parsed = loginDto.safeParse(req.body);
+      if (!parsed.success) {
+        return res.status(400).json({
+          success: false,
+          message: z.prettifyError(parsed.error),
+        });
+      }
+
+      const data = parsed.data;
+
+      // Call service
+      const { token, user } = await service.login(data.email, data.password);
+
+      // Send response
+      return res.status(200).json({
+        success: true,
+        message: "Login successful",
+        data: user, // user object without password
+        token,
+      });
+    } catch (error: any) {
+      return res.status(error.statusCode ?? 500).json({
+        success: false,
+        message: error.message || "Internal Server Error",
+      });
+    }
+  }
+}
